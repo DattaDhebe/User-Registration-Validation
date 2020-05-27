a348b0ae151ab924bb16c00a34c9b7099cb1fa75
DattaDhebe <dattadhebe75@gmail.com>
2020-05-28 01:20:53 +0530

    Added. As a User Need to Enter a Valid First Name.

--- TestUserRegistration/UnitTest1.cs
+++ TestUserRegistration/UnitTest1.cs
@@ -7,17 +7,37 @@ namespace TestUserRegistration
     public class Tests
     {
         [SetUp]
-        public void Setup()
-        {   
-        }
+        public void Setup() {   }
 
         UserValidation userValidation = new UserValidation();
 
         [Test]
-        public void Test1()
+        public void GivenProperName_ShouldReturnTrue()
         {
             Boolean result = userValidation.FirstName("Datta");
             Assert.True(result);
-        } 
+        }
+
+        [Test]
+        public void GivenLessThanThreeCharacter_ShouldReturnFalse()
+        {
+            Boolean result = userValidation.FirstName("Da");
+            Assert.False(result);
+        }
+
+        [Test]
+        public void GivenNumbersInName_ShouldReturnFalse()
+        {
+            Boolean result = userValidation.FirstName("Da324");
+            Assert.False(result);
+        }
+
+        [Test]
+        public void GivenEmptyName_ShouldReturnFalse()
+        {
+            Boolean result = userValidation.FirstName("");
+            Assert.False(result);
+        }
+
     }
 }
--- UserRegistration/UserRegistration.cs
+++ UserRegistration/UserRegistration.cs
@@ -5,7 +5,6 @@ namespace UserRegistration
 {
     public class UserValidation
     {
-
         public static String patterFor_firstName = "^[A-Z]{1}[a-z]{3,}$";
 
         public Boolean FirstName(String firstName)
